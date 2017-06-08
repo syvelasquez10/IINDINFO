4a1a73584ec4d28f2f320ad201d43d46d829dd81
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-06-08 15:08:13 -0500

    cors

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,12 @@ Bundler.require(*Rails.groups)
 
 module AppIndustrial
   class Application < Rails::Application
+    use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -30,3 +36,4 @@ module AppIndustrial
     config.api_only = true
   end
 end
+
