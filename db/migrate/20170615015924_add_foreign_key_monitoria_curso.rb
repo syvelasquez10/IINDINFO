c3fa7f11ea046777f479b23c6e55bb2f34e80f87
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-06-14 23:52:25 -0500

    error de migraciones

--- db/migrate/20170615015924_add_foreign_key_monitoria_curso.rb
+++ db/migrate/20170615015924_add_foreign_key_monitoria_curso.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyMonitoriaCurso < ActiveRecord::Migration[5.0]
   def change
-    add_reference :monitorias, :curso, foreign_key: true
+    /add_reference :monitorias, :curso, foreign_key: true/
   end
 end
