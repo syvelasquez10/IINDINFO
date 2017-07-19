103627ed8f1dcb03d66a31066f4798767a4aaa17
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-07-18 22:40:52 -0500

    error

--- app/controllers/estudiantes_controller.rb
+++ app/controllers/estudiantes_controller.rb
@@ -6,7 +6,7 @@ class EstudiantesController < ApplicationController
     @estudiantes = Estudiante.all
     estudiantes = []
     for estudiante in @estudiantes
-      estudiantes.push(estudiante.as_json.merge('monitorias':estudiante.monitorias.as_json).merge('practicas':estudiante.practicas.as_json).merge('saberPro':estudiante.saberPro.as_json).merge('citas':@estudiante.citas.as_json))
+      estudiantes.push(estudiante.as_json.merge('monitorias':estudiante.monitorias.as_json).merge('practicas':estudiante.practicas.as_json).merge('saberPro':estudiante.saberPro.as_json).merge('citas':estudiante.citas.as_json))
     end
     render json: estudiantes
   end
