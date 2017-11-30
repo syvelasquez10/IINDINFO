e21fc020152f5fc76423bb98f1e07499fe25e267
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2017-11-29 22:50:03 -0500

    Arreglo migrations

--- db/migrate/20170717205219_add_colum_estudiante_to_cita.rb
+++
@@ -1,5 +0,0 @@
-class AddColumEstudianteToCita < ActiveRecord::Migration[5.0]
-  def change
-    add_column :citas, :estudiante_id, :integer
-  end
-end
--- db/migrate/20170717205715_add_colum_administrador_to_cita.rb
+++
@@ -1,5 +0,0 @@
-class AddColumAdministradorToCita < ActiveRecord::Migration[5.0]
-  def change
-    add_column :citas, :administrador_id, :integer
-  end
-end
